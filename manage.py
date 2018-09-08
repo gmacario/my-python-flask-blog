203f106fea2c68d9c5b94edbff353fb3b0ebb085
Gianpaolo Macario <gmacario@gmail.com>
2018-09-08 11:10:12 +0200

    manage.py: Populate script

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
-# File: manage.py
+#!/usr/bin/env python
+
+from blog import create_app
+from flask_script import Manager
 
 print("DEBUG: START manage.py")
+
+app = create_app()
+manager = Manager(app)
+
+if __name__ == '__main__':
+    manager.run()
 
 print("DEBUG: END manage.py")
 
