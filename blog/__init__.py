4966762946f5e4c651bae1ee6f05017f1243a668
Gianpaolo Macario <gmacario@gmail.com>
2018-09-09 23:32:38 +0200

    Part2: NavBar
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 from flask import Flask
-
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
 
 bootstrap = Bootstrap()
-
-from .main import main as main_bp
+nav = Nav()
 
 def create_app():
     app = Flask(__name__)
     bootstrap.init_app(app)
+    nav.init_app(app)
+
+    from .main import main as main_bp
     app.register_blueprint(main_bp)
     return app
 
-# EOF
+from .navbar import main_nav
