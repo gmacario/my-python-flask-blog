8ccc2f4fd920a80a1c15851fb0a280760968a033
Gianpaolo Macario <gmacario@gmail.com>
2018-09-09 23:12:30 +0200

    Part2: Pagina principale, Pagine di errore
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,10 +2,15 @@
 
 from flask import Flask
 
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
 from .main import main as main_bp
 
 def create_app():
     app = Flask(__name__)
+    bootstrap.init_app(app)
     app.register_blueprint(main_bp)
     return app
 
--- blog/main/__init__.py
+++ blog/main/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, template_folder="templates")
 
 from . import views, errors
 
